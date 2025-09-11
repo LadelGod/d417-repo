4832e06f71db3bc43ca20e6c5746e60897b66e06
LadelGod <skullsword98@gmail.com>
2025-09-11 00:59:55 -0400

    Updates create-vlans.py to add the configurations to the other 4 switches

--- create-vlans.py
+++ create-vlans.py
@@ -54,6 +54,113 @@ def create_vlans() :
     newConfig = connection.send_command("show vlan")
     print(newConfig)
 
+    #Closes the connection with the switch
+    connection.disconnect()
+
+    #Prints the starting configuration
+    device = device3
+    connection = ConnectHandler(**device)
+    startingPoint = connection.send_command("show vlan")
+    print(startingPoint)
+
+    #Creates the User_Network VLAN
+    creation = connection.send_command("create vlan ACCT_Network tag 20")
+    print(creation)
+
+    #Adds ports & trunk to the User_network VLAN
+    addPorts = connection.send_command("configure vlan 20 add port 2 untagged")
+    addTrunk = connection.send_command("configure vlan 20 add port 1 tagged")
+    print(addPorts)
+    print(addTrunk)
+
+    #Prints the new configuration
+    newConfig = connection.send_command("show vlan")
+    print(newConfig)
+
+    #Closes the connection with the switch
+    connection.disconnect()
+
+    #Prints the starting configuration
+    device = device4
+    connection = ConnectHandler(**device)
+    startingPoint = connection.send_command("show vlan")
+    print(startingPoint)
+
+    #Creates the User_Network VLAN
+    creation = connection.send_command("create vlan MGMT_Network tag 30")
+    print(creation)
+
+    #Adds ports & trunk to the User_network VLAN
+    addPorts = connection.send_command("configure vlan 30 add port 2 untagged")
+    addTrunk = connection.send_command("configure vlan 30 add port 1 tagged")
+    print(addPorts)
+    print(addTrunk)
+
+    #Prints the new configuration
+    newConfig = connection.send_command("show vlan")
+    print(newConfig)
+
+    #Closes the connection with the switch
+    connection.disconnect()
+
+    #Prints the starting configuration
+    device = device5
+    connection = ConnectHandler(**device)
+    startingPoint = connection.send_command("show vlan")
+    print(startingPoint)
+
+    #Creates the User_Network VLAN
+    creation = connection.send_command("create vlan IT_Network tag 40")
+    print(creation)
+
+    #Adds ports & trunk to the User_network VLAN
+    addPorts = connection.send_command("configure vlan 40 add port 2 untagged")
+    addTrunk = connection.send_command("configure vlan 40 add port 1 tagged")
+    print(addPorts)
+    print(addTrunk)
+
+    #Prints the new configuration
+    newConfig = connection.send_command("show vlan")
+    print(newConfig)
+
+    #Closes the connection with the switch
+    connection.disconnect()
+
+    #Prints the starting configuration
+    device = device1
+    connection = ConnectHandler(**device)
+    startingPoint = connection.send_command("show vlan")
+    print(startingPoint)
+
+    #Creates the User_Network VLAN
+    creation = connection.send_command("create vlan User_Network tag 10")
+    print(creation)
+    creation = connection.send_command("create vlan ACCT_Network tag 20")
+    print(creation)
+    creation = connection.send_command("create vlan MGMT_Network tag 30")
+    print(creation)
+    creation = connection.send_command("create vlan IT_Network tag 40")
+    print(creation)
+    
+    #Adds ports & trunk to the User_network VLAN
+    addPorts = connection.send_command("configure vlan 40 add port 3 untagged")
+    print(addPorts)
+    addPorts = connection.send_command("configure vlan 30 add port 5 untagged")
+    print(addPorts)
+    addPorts = connection.send_command("configure vlan 20 add port 7 untagged")
+    print(addPorts)
+    addPorts = connection.send_command("configure vlan 10 add port 9 untagged")
+    print(addPorts)
+    addTrunk = connection.send_command("configure vlan 10,20,30,40 add port 12 tagged")
+    print(addPorts)
+    print(addTrunk)
+
+    #Prints the new configuration
+    newConfig = connection.send_command("show vlan")
+    print(newConfig)
+
+    #Closes the connection with the switch
+    connection.disconnect()
     return "New VLANs have been created"
 
 print(create_vlans())
