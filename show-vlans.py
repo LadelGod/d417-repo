3321a040220a55152de253bb78ad9b41f1a025e8
LadelGod <skullsword98@gmail.com>
2025-09-10 22:29:42 -0400

    Updates write to file block to work, maybe

--- show-vlans.py
+++ show-vlans.py
@@ -50,7 +50,7 @@ print(show_vlans(switchList))
 
 with open('vlans-list.txt', 'w') as f:
     try:
-        f.write(outputList + '\n')
+        f.write(outputList)
     except:
         print('An error has occured.')
     else:
